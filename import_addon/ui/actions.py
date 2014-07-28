0dffec937c9973d5dc8f40026ec390272febeffb
Oscar Newman <onewman@Mavericks-Dev-Test.local>
2014-07-28 16:32:05 -0500

    Began to put UI & Options into separate package

--- import_addon/ui/actions.py
+++ import_addon/ui/actions.py
@@ -9,8 +9,8 @@ from pyface.action.api import Action
 from pyface.api import ConfirmationDialog, FileDialog, \
     YES, OK, CANCEL
 from canopy.file_handling.i_file_handling_service import IFileHandlingService
+import table_import
 
-import application_manager
 
 class OpenTableAction(TaskAction):
 
@@ -22,9 +22,9 @@ class OpenTableAction(TaskAction):
     }
 
     file_type_name = "File"
-    name = 'Import DataFrame From File...'
+    name = 'Import DataFrame...'
     tooltip = name
-    id = 'OpenFileAction'
+    id = 'OpenTableAction'
 
     def perform(self, event):
         app = event.task.window.application
@@ -35,26 +35,8 @@ class OpenTableAction(TaskAction):
             wildcard=self._wildcard_string(), default_directory=directory)
         dialog.title = 'Load Table to UI Builder'
         if dialog.open() == OK:
-            ext = os.path.splitext(dialog.path)[1]
-            self.extension = ext
             service.push_recent_file(dialog.path)
-
-            opts = self._get_model(dialog.path)
-            opts.path = dialog.path
-            opts.application = app
-            opts.save_dir = app.home
-
-            application_manager.create_app()
-
-            view = self._get_enaml_view(dialog.path)
-            view.options = opts
-            view.show()
-
-    def _get_enaml_view(self, path):
-        raise NotImplementedError('Enaml View Not Defined')
-
-    def _get_model(self, path):
-        raise NotImplementedError('Model Not Defined')
+            table_import.run(dialog.path, app.home)
 
     def _wildcard_string(self):
         wildcard = ""
@@ -64,85 +46,9 @@ class OpenTableAction(TaskAction):
         return wildcard[:-1]
 
 
-class OpenDelimFixedAction(OpenTableAction):
-    """ Open a Delimted or Fixed Width file. """
-
-    file_type_name = "All"
-    file_types = {
-        'All Files(*)': '*',
-    }
-
-    name = 'Import DataFrame...'
-    tooltip = name
-    id = 'OpenDelimFixedAction'
-
-    xl = ['.xls', '.xlsx']
-
-
-    def _get_enaml_view(self, path):
-        if self.extension.lower() in self.xl:
-            with traits_enaml.imports():
-                from excel_import_view import ExcelImportView
-            return ExcelImportView()
-
-        with traits_enaml.imports():
-                from delim_fixed_import_view import DelimFixedImportView
-        return DelimFixedImportView()
-
-    def _get_model(self, path):
-        if self.extension.lower() in self.xl:
-            from excel_import_options import ExcelImportOptions
-            return ExcelImportOptions()
-
-        from delim_fixed_import_options import DelimFixedImportOptions
-        return DelimFixedImportOptions()
-
-class OpenFWFAction(OpenTableAction):
-    """ Open a Fixed Width file. """
-
-    file_type_name = "Fixed Width File"
-    file_types = {
-        'All files (*)' : '*',
-    }
-
-    name = 'Fixed Width File'
-    tooltip = name
-    id = 'OpenFWFAction'
-
-    def _get_enaml_view(self):
-        with traits_enaml.imports():
-                from fwf_import_view import FWFImportView
-        return FWFImportView()
-
-    def _get_model(self):
-        from fwf_import_options import FWFImportOptions
-        return FWFImportOptions()
-
-class OpenExcelAction(OpenTableAction):
-    """ Open a Fixed Width file. """
-
-    file_type_name = "Excel"
-    file_types = {
-        'Excel Files (*.xls)' : '*.xls',
-    }
-
-    name = 'Excel'
-    tooltip = name
-    id = 'OpenExcelAction'
-
-    def _get_enaml_view(self):
-        with traits_enaml.imports():
-                from excel_import_view import ExcelImportView
-        return ExcelImportView()
-
-    def _get_model(self):
-        from excel_import_options import ExcelImportOptions
-        return ExcelImportOptions()
-
 
 
 menu_group_schema = ActionSchema(
-    action_factory=OpenDelimFixedAction,
-    id='OpenDelimFixedAction',
-    name = 'Import Dataframe...'
+    action_factory=OpenTableAction,
+    id='OpenTableAction',
 )
